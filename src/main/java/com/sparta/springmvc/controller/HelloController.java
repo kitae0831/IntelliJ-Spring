b5b668a6ce0561a4cf6c3178fd3b9387fb91a180
KitaePark <bkt4044@naver.com>
2024-04-04 13:40:17 +0900

    Controller 이해하기

--- src/main/java/com/sparta/springmvc/controller/HelloController.java
+++ src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.sparta.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "Hello World!!";
@@ -14,9 +14,27 @@ public class HelloController {
 
 
 
-    @GetMapping("/api/get")
+    @GetMapping("/get")
     @ResponseBody
     public String get() {
         return "Get Method 요청";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "POST Method 요청";
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "PUT Method 요청";
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "DELETE Method 요청";
     }
 }
